6e639faf971d631f90780c73c13687cfe078010a
NetoDamasceno <luiznetto13@gmail.com>
2026-02-10 22:08:00 -0300

    add cart.selectors.js and total price to cart

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,6 +8,7 @@ import Cart from "../cart/index";
 import * as Styles from "./styles";
 
 import { loginUser, logoutUser } from "../../redux/user/actions";
+import { selectProductsCount } from "../../redux/cart/cart.selectors";
 
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
@@ -15,11 +16,10 @@ function Header() {
   const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
   const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
 
+  const productsCount = useSelector(selectProductsCount)
+
   const dispatch = useDispatch();
 
-  const productsCount = useMemo(() => {
-    return products.reduce((acc, curr) => acc + curr.quantity, 0);
-  }, [products]);
 
   const handleCartClick = () => {
     setCartIsVisible(true);
